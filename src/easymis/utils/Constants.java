e5cdbbe8b53982c385887f32c66e059d58db6f74
faisal <faisal@faisalspc>
2019-06-12 23:54:05 -0700

    changes as on 6/7

--- src/easymis/utils/Constants.java
+++ src/easymis/utils/Constants.java
@@ -10,4 +10,9 @@ public class Constants {
     public static final String SIDE_MENU_VIEW = "/easymis/views/SideMenuView.fxml";
     public static final String EVENT_BOOKING_VIEW = "/easymis/views/EventBookingView.fxml";
     public static final String DASHBOARD_VIEW = "/easymis/views/Dashboard.fxml";
+    public static final String EMPLOYEE_VIEW = "/easymis/views/Employee.fxml";
+    public static final String EXPENSES_VIEW = "/easymis/views/Expenses.fxml";
+    public static final String INVENTORY_VIEW = "/easymis/views/Inventory.fxml";
+    public static final String REPORT_VIEW = "/easymis/views/Reports.fxml";
+    public static final String SETTINGS_VIEW = "/easymis/views/Settings.fxml";        
 }
